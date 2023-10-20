67679ff71debd270aeb35d651b0d04cc60f7b6b2
Andimass <masyuckevich12345@gmail.com>
2023-10-20 18:29:00 +0300

    7 commit

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default {
+  server: {
+    host: 'partner-tech.ru', // Или IP-адрес 89.108.77.107
+    port: 80, // Порт, который вы хотите использовать
+  },
+};
