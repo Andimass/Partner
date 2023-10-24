6f832c9fe4d616a8b523a5f97d417ae425eec640
Andimass <masyuckevich12345@gmail.com>
2023-10-24 09:03:18 +0300

    7 commit

--- src/components/screens/home/About/About.jsx
+++ src/components/screens/home/About/About.jsx
@@ -59,10 +59,10 @@ const About = () =>  {
     return(
         <head>
          <title>О компании</title>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-        <link rel="manifest" href="/site.webmanifest">
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
         <div className='About'>
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <div className={`supply_header ${activeHandler === 4 ? 'bgc_opacity' : ''}`}>
--- src/components/screens/home/Header/Header.jsx
+++ src/components/screens/home/Header/Header.jsx
@@ -169,10 +169,10 @@ const Header = () => {
         return (
         <head>
          <title>Партнер</title>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-        <link rel="manifest" href="/site.webmanifest">
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
             <div className='Header_glav'>
                 <span className="circle"></span>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
